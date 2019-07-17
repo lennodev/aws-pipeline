84503336b6d4f484d0dc30b6f1c4f07df751730c
lennodev <lennodev@gmail.com>
2019-07-17 14:24:27 +0800

    Add even no. test case

---
+++ __test__/evenNumber.test.js
@@ -0,0 +1,13 @@
+function isEven(num) {
+	
+  if (num % 2 === 0) {
+	  return false;
+  }else{
+	  return true;
+}
+
+describe("test even number", () => {
+  it("4 is a even number", () => {
+    expect(isOdd(4)).toBe(true);
+  });
+});
